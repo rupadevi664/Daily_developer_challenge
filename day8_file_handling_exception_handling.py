527b5bdf325d0004c97ab281dfc892b19f289369
rupadevi664 <rupadevigodi@gmail.com>
2026-09-08 19:44:09 +0530

    # ignone

--- day8_file_handling_exception_handling.py
+++ day8_file_handling_exception_handling.py
@@ -7,47 +7,48 @@ with open("student.txt","w") as file:
     file.write("durga\n")
 print("students added succesfully")
 
-# with open("student.txt","r") as file:
-#     for students in file:
-#         print(students.strip())
+with open("student.txt","r") as file:
+    for students in file:
+         print(students.strip())
 
 # #append
-# with open("student.txt","a") as file:
-#     file.write("vijay\n")
-#     file.write("hii\n")
-#     file.write("hello\n")
-# print("Student appended succesfully")
+with open("student.txt","a") as file:
+     file.write("vijay\n")
+     file.write("hii\n")
+     file.write("hello\n")
+print("Student appended succesfully")
 
-# #handling file not found KeyError
-# try:
-#     with open("unknown.txt","r") as file:
-#         data=file.read()
-#         print(data)
-# except FileNotFoundError:
-#     print("file not found")
+ #handling file not found KeyError
+'''try:
+    with open("unknown.txt","r") as file:
+        data=file.read()
+        print(data)
+except FileNotFoundError:
+     print("file not found")
 
 # # handles errors vaild numbers,zero diviso=ion error,invalid operarion by persfoming calculation operation
-# try:
-#     num1=int(input("enter number:"))
-#     num2=int(input("enter number:"))
-#     operation=input("enter operation +,-,*,/")
+try:
+     num1=int(input("enter number:"))
+     num2=int(input("enter number:"))
+     operation=input("enter operation +,-,*,/")
 
-#     if operation=="+":
-#         print("result",num1+num2)
-#     elif operation=="-":
-#         print("result",num1-num2)
-#     elif operation=="*":
-#         print("result",num1*num2)
-#     elif operation=="/":
-#         print("result",num1/num2)
-#     else:
-#         raise ValueError("entered invalid operation") 
-# except ValueError:
-#         print("invalid number")
-# except ZeroDivisionError:
-#     print("can not divided by zero") 
+     if operation=="+":
+         print("result",num1+num2)
+     elif operation=="-":
+         print("result",num1-num2)
+     elif operation=="*":
+         print("result",num1*num2)
+     elif operation=="/":
+         print("result",num1/num2)
+     else:
+         raise ValueError("entered invalid operation") 
+except ValueError:
+         print("invalid number")
+except ZeroDivisionError:
+        print("can not divided by zero") 
 
 #validate age and raise error
+
 def validate_age(age):
     if age<18:
         print("invalid age")
@@ -57,7 +58,34 @@ try:
     num=int(input("enter number"))
     validate_age(num)
 except ValueError as e:
-    print("error",e)
+    print("error",e)'''
 
+#virtual enviroment:it is a seperate python environment used to manage diffent python packages and versions without effecting with other projects
 
+# module:it is a python file contain reusable code such as functions,classes,variables.class
 
+#packages:a folder containing mulitple modules such as .py files
+
+#requirements.txt: pip freeze > requirements.txt 
+#if others want to install our file: pip install -r requirements.txt
+
+with open("student.txt","r") as file:
+    line=file.readline()
+    print(line)
+
+with open("student.txt","r") as file:
+    line=file.readlines()
+    print(line)
+# output is in list format
+
+with open("student.txt","w") as file:
+    lines=["hii\n","hell\n","hoo\n"]
+    file.writelines(lines)
+    print(lines)
+#exception handling: exception handling means it is a mechanisms handling exceptions so that it will terminate gracefully instead of crashing the prg
+# try: it contains code that may cause errror
+# except: it handles the except
+# raise: we manually write the errrors
+# finally: it runs if contains errror or not
+# keyerror,indexerror,typeerror-when we add to different strings,valueerror,attributeerrror
+# import error,zero division error,file not found errror 
